7f791c5c1bc19af85c6074dceb49ca17daae5778
Ivan <ivanezeh12@gmail.com>
2024-10-29 18:54:35 +0100

    one track minded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,10 +31,20 @@ export default function Home() {
     const fetchData = async () => {
       try {
           const response = await api.get("./post")
+          setPosts(response.data) 
+          console.log(`poop`)
         } catch (err){
-
+          if(err.response){
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+          }else{
+            console.log(`Error: ${err.message}`)
+          }
       }
     }
+
+    fetchData()
   }, [])
 
   return (
